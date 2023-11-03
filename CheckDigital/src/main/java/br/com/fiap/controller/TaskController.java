8661d055e28802fcbc182dd4204542778d36b6b5
RenanCCastilho <144824790+RenanCCastilho@users.noreply.github.com>
2023-11-03 14:33:35 -0300

    Add files via upload

---
+++ CheckDigital/src/main/java/br/com/fiap/controller/TaskController.java
@@ -0,0 +1,51 @@
+package br.com.fiap.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import br.com.fiap.model.Task;
+import br.com.fiap.service.TaskService;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+	private final TaskService taskService;
+
+	public TaskController(TaskService taskService) {
+		this.taskService = taskService;
+	}
+
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	public List<Task> getAllTasks() {
+		return taskService.getAllTasks();
+	}
+
+	@GetMapping("/{taskId}")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	public Task getTaskById(@PathVariable Long taskId) {
+		return taskService.getTaskById(taskId);
+	}
+
+	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public Task createTask(@RequestBody Task task) {
+		return taskService.createTask(task);
+	}
+
+	@PutMapping("/{taskId}")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	public Task updateTask(@PathVariable Long taskId, @RequestBody Task task) {
+		return taskService.updateTask(taskId, task);
+	}
+
+	@DeleteMapping("/{taskId}")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public void deleteTask(@PathVariable Long taskId) {
+		taskService.deleteTask(taskId);
+	}
+
+}
---
+++ CheckDigital/src/main/java/br/com/fiap/model/UserInfoDetails.java
@@ -0,0 +1,68 @@
+package br.com.fiap.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInfoDetails implements UserDetails {
+
+	private String username;
+	private String password;
+	private List<GrantedAuthority> authorities;
+
+	public UserInfoDetails(User userInfo) {
+		username = userInfo.getUsername();
+		password = userInfo.getPassword();
+		authorities = Arrays.stream(userInfo.getRole().split(",")).map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+	}
+
+	public UserInfoDetails() {
+
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
